fdc25fc1bf0b081b2cf4c3d52d9aa49c53b38b01
Zernach <Ryan@Zernach.com>
2020-11-02 13:22:44 -0500

    november refresher

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 
 # Imports from this application
 from app import app, server
-from pages import index, predictions, insights, process
+from pages import index, predictions, process
 
 # Navbar docs: https://dash-bootstrap-components.opensource.faculty.ai/l/components/navbar
 navbar = dbc.NavbarSimple(
